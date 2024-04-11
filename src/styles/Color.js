aab02662f51800db6131c8a2d9357e43641b87d9
heyejiyang <xoxo99312@naver.com>
2024-04-11 16:30:16 +0900

    공통 스타일, 공통 글자크기, 공통 색상 스타일링중...

---
+++ src/styles/Color.js
@@ -0,0 +1,21 @@
+export const color = {
+  primary: '#007bff',
+  secondary: '#6c757d',
+  success: '#28a745',
+  danger: '#dc3545',
+  warning: '#ffc107',
+  info: '#17a2b8',
+  light: '#f8f9fa',
+  dark: '#343a40',
+};
+
+export const butonColor = {
+  primary: [color.primary, '#fff'], //두번째꺼는 글자색
+  secondary: [color.secondary, '#fff'],
+  success: [color.success, '#fff'],
+  danger: [color.danger, '#fff'],
+  warning: [color.warning, '#212529'],
+  info: [color.info, '#fff'],
+  light: [color.info, '#212529'],
+  dark: [color.dark, '#fff'],
+};
